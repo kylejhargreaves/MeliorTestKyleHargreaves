239536ea30493872813b2cd12d3444156bc28bed
kylejhargreaves <102608293+kylejhargreaves@users.noreply.github.com>
2025-03-16 12:43:03 +0000

    Add some basic methods to interface to segregate some logic away from the payment service

--- Melior.InterviewQuestion/Types/Account.cs
+++ Melior.InterviewQuestion/Types/Account.cs
@@ -1,11 +1,26 @@
 ﻿namespace Melior.InterviewQuestion.Types
 {
-    public class Account
+    public class Account:IAccount
     {
         public string AccountNumber { get; set; }
         public decimal Balance { get; set; }
         public AccountStatus Status { get; set; }
         public AllowedPaymentSchemes AllowedPaymentSchemes { get; set; }
+
+        public bool CreditAccount(decimal amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DebitAccount(decimal amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool VerifyAccount(PaymentScheme paymentScheme)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
 
---
+++ Melior.InterviewQuestion/Types/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melior.InterviewQuestion.Types
+{
+    public interface IAccount
+    {
+        public bool DebitAccount(Decimal amount);
+        public bool CreditAccount(Decimal amount);
+        public bool VerifyAccount(PaymentScheme paymentScheme);
+    }
+}
